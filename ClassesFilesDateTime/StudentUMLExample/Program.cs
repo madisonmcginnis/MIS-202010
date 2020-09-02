408184a44ab47cf449c272ddf682cd5ac2ac0eb2
Madison McGinnis <mmcginnis130@ou.edu>
2020-09-02 12:21:29 -0600

    Created a student and displayed name, GPA, probation status, and bursar balance

--- ClassesFilesDateTime/StudentUMLExample/Program.cs
+++ ClassesFilesDateTime/StudentUMLExample/Program.cs
@@ -13,7 +13,24 @@ namespace StudentUMLExample
     {
         static void Main(string[] args)
         {
-            
+            Student myStudent = new Student();
+            Console.WriteLine(myStudent);
+            myStudent.FirstName = "Madison";
+            myStudent.LastName = "McGinnis";
+            myStudent.IsOnProbation = false;
+            myStudent.SoonerID = 5;
+
+            Console.WriteLine(myStudent);
+
+            myStudent.MakePayment(500);
+            Console.WriteLine(myStudent);
+
+            myStudent.MakePayment(50);
+            Console.WriteLine(myStudent);
+
+            myStudent.MakePayment(500);
+            Console.WriteLine(myStudent);
+
         }
     }
 }
--- ClassesFilesDateTime/StudentUMLExample/Student.cs
+++ ClassesFilesDateTime/StudentUMLExample/Student.cs
@@ -6,6 +6,91 @@ namespace StudentUMLExample
 {
     class Student
     {
+        public int SoonerID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool IsOnProbation { get; set; }
+        public double GPA { get; set; }
 
+        private double BursarBalance;
+
+        /// <summary>
+        /// Default/Empty constructor for the Student
+        /// </summary>
+        public Student()
+        {
+            SoonerID = -1;
+            FirstName = string.Empty;
+            LastName = "";
+            IsOnProbation = false;
+            GPA = 0;
+            BursarBalance = 1000;
+        }
+
+        /// <summary>
+        /// overloaded constructor that will set the default values of soonerid, firstname, lastname, and bursarbalance
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="fName"></param>
+        /// <param name="lName"></param>
+        /// <param name="BursarBalance"></param>
+        public Student(int id, string fName, string lName, double BursarBalance)
+        {
+            SoonerID = id;
+            FirstName = fName;
+            LastName = lName;
+            IsOnProbation = false;
+            GPA = 0;
+            this.BursarBalance = BursarBalance;
+        }
+
+        /// <summary>
+        /// Make a payment on your bursar balance
+        /// </summary>
+        /// <param name="amount"></param>a non-negative value to reduce bursar balance
+        public void MakePayment(double amount)
+        {
+            //BursarBalance -= amount;
+            if (amount > 0)
+            {
+                BursarBalance = BursarBalance - amount;
+            }
+            else
+            {
+                throw new Exception();
+            }
+            
+        }
+
+        /// <summary>
+        /// the current state of your bursar account
+        /// </summary>
+        /// <returns></returns> how much you owe on your bursar
+        public double CheckBalance()
+        {
+            return BursarBalance;
+        }
+
+        /// <summary>
+        /// create a string representation of our instance of the class
+        /// </summary>
+        /// <returns>the current state of the student</returns>
+        public override string ToString()
+        {
+            string output = $"{FirstName} {LastName} ({SoonerID})";
+            if (IsOnProbation == true)
+            {
+                output += " is on probation.\n";
+            }
+            else
+            {
+                output += " is not on probation.\n";
+            }
+            output += $"They have a {GPA.ToString("N2")} GPA and they owe {BursarBalance.ToString("C2")}\n";
+
+            
+
+            return output;
+        }
     }
 }
