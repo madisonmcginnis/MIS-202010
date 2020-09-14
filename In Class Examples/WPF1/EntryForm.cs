2497348f75f155f3ae9f90ca0c4baec29cb868b3
Madison McGinnis <mmcginnis130@ou.edu>
2020-09-14 12:34:49 -0600

    Created the main window design and assigned variables to what the user puts in the text box. Also added a mouse enter and exit color change

---
+++ In Class Examples/WPF1/EntryForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPF1
+{
+    class EntryForm
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int ZipCode { get; set; }
+
+        public EntryForm()
+        {
+
+        }
+        public EntryForm(string name, string address, int zipCode)
+        {
+            name = Name;
+            address = Address;
+            zipCode = ZipCode;
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
