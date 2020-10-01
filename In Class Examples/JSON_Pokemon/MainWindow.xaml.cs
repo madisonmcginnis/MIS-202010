ac1f8e29aee592a4cf9479e9c2f72f9ee3006907
Madison McGinnis <mmcginnis130@ou.edu>
2020-10-01 15:32:11 -0600

    added all the pokemons to the combo box and outputted height and weight into label

---
+++ In Class Examples/JSON_Pokemon/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JSON_Pokemon
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string webService = @"https://pokeapi.co/api/v2/pokemon?limit=1100&offset=0";
+
+            Pokemon characters;
+
+            using (var client = new HttpClient())
+            {
+                var results = client.GetStringAsync(webService).Result;
+
+                characters = JsonConvert.DeserializeObject<Pokemon>(results);
+            }
+
+            foreach (var character in characters.results)
+            {
+                cboPokemon.Items.Add(character);
+            }
+        }
+
+        private void cboPokemon_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var character = (Pokemons)cboPokemon.SelectedItem;
+
+            string newWebService = $"{character.url}";
+
+            Details pokemon;
+
+            using (var clientTwo = new HttpClient())
+            {
+                var pokemonResults = clientTwo.GetStringAsync(newWebService).Result;
+
+                pokemon = JsonConvert.DeserializeObject<Details>(pokemonResults);
+            }
+
+            lblInfo.Content = $"{pokemon.ToString()}";
+            //imgPokemon.Source = new BitmapImage(new Uri(pokemon.frontImage));
+
+            MessageBox.Show($"{pokemon.back_default}");
+        }
+    }
+}
---
+++ In Class Examples/JSON_Pokemon/Pokemon.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSON_Pokemon
+{
+    class Pokemon
+    {
+        public Info info { get; set; }
+
+        public List<Pokemons> results { get; set; }
+    }
+
+    public class Pokemons
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+
+        public override string ToString()
+        {
+            return name;
+        }
+    }
+
+    public class Info
+    {
+        public int count { get; set; }
+        
+        public string next { get; set; }
+        public string previous { get; set; }
+    }
+}
