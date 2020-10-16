4f233f7c3bc89801d8c285d862903694fd3e8fa8
Madison McGinnis <mmcginnis130@ou.edu>
2020-10-16 14:37:37 -0600

    Copied over the classes from the Graduation handout and created a new window

---
+++ In Class Examples/WPF_Classes3/Address.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraduationHandout
+{
+    class Address
+    {
+        public int StreetNumber { get; set; }
+        public string StreetName { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+        public int Zipcode { get; set; }
+
+
+        public Address()
+        {
+            StreetNumber = 0;
+            StreetName = "";
+            State = "";
+            City = "";
+            Zipcode = 0;
+        }
+
+        public Address(int streetnumber, string streetname, string state, string city, int zipcode)
+        {
+            streetnumber = StreetNumber;
+            streetname = StreetName;
+            state = State;
+            city = City;
+            zipcode = Zipcode;
+        }
+    }
+}
---
+++ In Class Examples/WPF_Classes3/Student.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraduationHandout
+{
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Major { get; set; }
+        public double GPA { get; set; }
+        public Address Address { get; set; }
+
+        string distinction;
+
+        public Student()
+        {
+            FirstName = "";
+            LastName = "";
+            Major = "";
+            GPA = 0;
+        }
+        public Student(string firstName, string lastName, string major, double gpa)
+        {
+            firstName = FirstName;
+            lastName = LastName;
+            major = Major;
+            gpa = GPA;
+        }
+        public string CalculateDistinction()
+        {
+            if (GPA >= 3.40 && GPA <= 3.59)
+            {
+                distinction = "cum laude";
+            }
+            else if (GPA >= 3.60 && GPA <= 3.79)
+            {
+                distinction = "magna cum laude";
+            }
+            else if (GPA >= 3.80 && GPA <= 4.00)
+            {
+                distinction = "summa cum laude";
+            }
+            else
+            {
+                distinction = "none";
+            }
+
+            return distinction;
+        }
+
+        public void SetAddress(int streetNumber, string streetName, string state, string city, int zipcode)
+        {
+            streetNumber = Address.StreetNumber;
+            streetName = Address.StreetName;
+            state = Address.State;
+            city = Address.City;
+            zipcode = Address.Zipcode;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}, {Major}, {distinction}";
+        }
+    }
+}
